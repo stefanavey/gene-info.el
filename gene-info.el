4b64579a81b75693cd192f5fc62d37249c442ebb
spa23 <stefan.avey@yale.edu>
2015-07-31 13:59:28 -0400

    function works but automatic recognition of entrezGeneIDs does not work. Still very slow

--- gene-info.el
+++ gene-info.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Stefan Avey
 
 ;; Author: Stefan Avey <stefan.avey@yale.edu>
-;; Version: 0.0.1000
+;; Version: 0.0.1001
 ;; Package-Requires: ((shr))
 ;; Keywords: bioinformatics
 ;; URL: https://github.com/stefanavey/gene-info.el
@@ -27,8 +27,12 @@
 
 ;; Provides a way to see gene summary information inside Emacs
 
+;; Main function is `gene-info-search` which searches the thing at point against NCBI's gene database and returns suggestions for what gene you might mean. It then downloads the summary for that gene as plain text into a new buffer.
+
 ;; TODO: 
 ;; - Add abiilty to check if `thing-at-point` is already an Entrez Gene ID (if so, don't need first step)
+;; - Could try using Entrez Programming Utlities http://www.ncbi.nlm.nih.gov/books/NBK25501/
+ 
 
 ;;; Code:
 
@@ -56,38 +60,46 @@
     (move-to-column c)
     (point)))
 
-(defun spa-find-gene-info ()
-  "If an html file exists in the database for the geneID, open it in view mode inside emacs.
-   Otherwise, open a browser to view gene annotation from Gene Cards for the gene symbol near the point"
+(defun gene-info-search (&optional organism)
+  "If the `thing-at-point` is a positive integer, search NCBI for gene annotations using the `thing-at-point` as the Entrez Gene ID. Otherwise, search NCBI genes with keyword given by `thing-at-point` and return a buffer with possible completions and prompt for correct gene ID.  In both cases, a new buffer named *gene-info:<ID>* (where <ID> is the Entrez Gene ID) is created and the full report on the gene is displayed.
+
+Organism specifies what organism to search for genes in. This defaults to 'human' if no value is supplied
+"
   (interactive)
-  (let (geneName geneID url buffList buffContents allNames buffName)
-    (setq geneName (thing-at-point 'word))
-    ;; Not a great solution becaus it is redundant but could make this part a function
-    (if (and (equal (nth 0 (re-seq "[0-9]+" geneName)) geneName) (> (string-to-number geneName) 0) )
-	(lambda ()
-	  (setq geneID geneName)
-	  (setq url2 (concatenate 'string "http://www.ncbi.nlm.nih.gov/gene/" 
-				  geneID "?report=full_report&format=text"))
-	  (describe-variable 'geneName)
-	  ;; (describe-variable 'url2)))
-	  (browse-url-emacs url2) ;(url-encode-url url2)) 
-	  (setq buffName (current-buffer))
-	  (kill-buffer "*html*")
-	  ;; (pop-to-buffer "*html*")
-	  ;; (csv-mode)       ; this won't turn off for some reason!
-	  ;; (view-mode -1)
-	  ;; (read-only-mode -1)
-	  (shr-render-buffer buffName)
-	  (kill-buffer buffName)
-	  (setq buffName (concatenate 'string "*gene-info:" geneID "*"))
-	  (if (get-buffer buffName)
-	      (kill-buffer buffName))
-	  (rename-buffer buffName)
-	  (view-mode))
+  (if organism
+      nil
+    (defvar organism "human")
+    )
+  (defvar geneName (thing-at-point 'word))
+  (let (geneID url buffList buffContents allNames buffName)
+    ;; Not a great solution because it is redundant but could make this part a function to reduce redundancy
+    ;; (if (string-match "\\`[1-9][0-9]*\\'" geneName) ; test whether geneName is a positive integer
+        ;; (and (equal (nth 0 (re-seq "[0-9]+" geneName)) geneName) (> (string-to-number geneName) 0) )
+    	;; (lambda ()
+    	  ;; (setq geneID geneName)
+    	  ;; (setq url2 (concatenate 'string "http://www.ncbi.nlm.nih.gov/gene/" 
+    	  ;; 			  geneID "?report=full_report&format=text"))
+    	  ;; (describe-variable 'geneID)
+    	  ;; ;; (describe-variable 'url2)))
+    	  ;; (browse-url-emacs url2) ;(url-encode-url url2)) 
+    	  ;; (setq buffName (current-buffer))
+    	  ;; (kill-buffer "*html*")
+    	  ;; ;; (pop-to-buffer "*html*")
+    	  ;; ;; (csv-mode)       ; this won't turn off for some reason!
+    	  ;; ;; (view-mode -1)
+    	  ;; ;; (read-only-mode -1)
+    	  ;; (shr-render-buffer buffName)
+    	  ;; (kill-buffer buffName)
+    	  ;; (setq buffName (concatenate 'string "*gene-info:" geneID "*"))
+    	  ;; (if (get-buffer buffName)
+    	  ;;     (kill-buffer buffName))
+    	  ;; (rename-buffer buffName)
+    	  ;; (view-mode))
+	;; )
       (setq url (concatenate 'string 
-			     "http://www.ncbi.nlm.nih.gov/gene?report=full_report&format=text&term=(" 
+			     "http://www.ncbi.nlm.nih.gov/gene?report=tabular&format=text&term=(" 
 			     geneName ") AND human[Organism]" ))
-      ;; (setq url (concatenate 'string "http://www.genecards.org/Search/Keyword?queryString=" geneName)) 
+      ;; (setq url (concatenate 'string "http://www.genecards.org/Search/Keyword?queryString=" geneName))
       ;; Find the file in my local database of HTML files it exists and open it in View Mode
       ;; other window; otherwise, just open the url in the browser                                     
       (require 'shr)
@@ -124,12 +136,13 @@
 	   ;; (describe-variable 'buffContents)
 	   (setq buffList (split-string buffContents "\n" t)))
 	 ;; (describe-variable 'buffList)))
-	 (let ( (result ()) )
+	 (let ( (result ()) prompt)
 	   (dolist (line buffList)
 	     (setq tmp (split-string line "\t" t))
 	     (push (nth 2 tmp) result))
 	   (setq result (nreverse result))
-	   (setq geneID (ido-completing-read "Ambiguous Search Term, Select GeneID: " result nil t nil nil nil nil))
+	   (setq prompt (concatenate 'string "Ambiguous Search Term '" geneName "', Select GeneID: "))
+	   (setq geneID (ido-completing-read prompt result nil t nil nil nil nil))
 	   (setq url2 (concatenate 'string "http://www.ncbi.nlm.nih.gov/gene/" 
 				   geneID "?report=full_report&format=text")))
 	 ;; (describe-variable 'url2)))
@@ -147,30 +160,15 @@
 	     (kill-buffer buffName))
 	 (rename-buffer buffName)
 	 (view-mode)
-	 )))
-    ))
+	 (makunbound 'geneName)
+	 (makunbound 'organism)
+	 )
+       ))
+    ;; )
+)
+(global-set-key "\C-cg" 'gene-info-search-test)
 
-
-
-;; 10
-;;   )
-;; ;; (browse-url url)
-;; ))
-;; (global-set-key "\C-cg" 'spa-find-gene-info)
-;; Try to be smart about which gene information to show
-;; Match this regular expression in the buffer string and return a list
-
-
-;; (let( (result ()) )
-;;      (push "hello" result)
-;;      (describe-variable 'result))
-
-;; Using NCBI
-;; Put contents of buffer into a list
-
-
-
-;; ;; Using Gene Cards
+;; ;; Using Gene Cards instead of NCBI
 ;; (setq temp (re-seq "data-ga-label=\".+\"" (buffer-substring-no-properties (point-min) (point-max))))
 ;; (setq geneName "IFNB")
 ;;  (let (url geneName (result  ()))
@@ -185,11 +183,45 @@
 ;;       (describe-variable 'url)
 ;;       (browse-url-emacs url))
 
+(defun gene-info-search-test (&optional organism)
+  "Debugging"
+  (interactive)
+  ;; (if organism
+  ;;     nil
+  ;;   (defvar organism "human")
+  ;;   )
+  (defvar geneName (thing-at-point 'word))
+  (let (geneID url buffList buffContents allNames buffName)
+    ;; Not a great solution becaus it is redundant but could make this part a function
+    (if (string-match "\\`[1-9][0-9]*\\'" geneName) ; test whether geneName is a positive integer
+	(lambda ()
+    	  (setq geneID geneName)
+    	  (setq url2 (concatenate 'string "http://www.ncbi.nlm.nih.gov/gene/" 
+    	  			  geneID "?report=full_report&format=text"))
+    	  ;; (describe-variable 'geneID)
+    	  ;; (describe-variable 'url2)))
+	  (message url2)
+    	  ;; (browse-url-emacs url2) ;(url-encode-url url2)) 
+    	  ;; (setq buffName (current-buffer))
+    	  ;; (kill-buffer "*html*")
+    	  ;; ;; (pop-to-buffer "*html*")
+    	  ;; ;; (csv-mode)       ; this won't turn off for some reason!
+    	  ;; ;; (view-mode -1)
+    	  ;; ;; (read-only-mode -1)
+    	  ;; (shr-render-buffer buffName)
+    	  ;; (kill-buffer buffName)
+    	  ;; (setq buffName (concatenate 'string "*gene-info:" geneID "*"))
+    	  ;; (if (get-buffer buffName)
+    	  ;;     (kill-buffer buffName))
+    	  ;; (rename-buffer buffName)
+    	  ;; (view-mode))
+	  )
+      nil
+      )
+        ;; (and (equal (nth 0 (re-seq "[0-9]+" geneName)) geneName) (> (string-to-number geneName) 0) )
+))
+(global-set-key "\C-cd" 'gene-info-search-test)
 
 (provide 'gene-info)
 
 ;;; gene-info.el ends here
-
-
-
-;; IFNB1
